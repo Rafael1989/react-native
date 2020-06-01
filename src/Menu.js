8e9273d99f80ee4c924b49cf2bcd4678ca3a452a
unknown <flns_rafa@hotmail.com>
2020-06-01 09:26:25 +0100

    Lista Flex

--- src/Menu.js
+++ src/Menu.js
@@ -9,6 +9,9 @@ import Contador from './componentes/Contador'
 import Plataforma from './componentes/Plataforma'
 import ValidarProps from './componentes/ValidarProps'
 import Evento from './componentes/Evento';
+import Avo from './componentes/ComunicacaoDireta';
+import TextoSincronizado from './componentes/ComunicacaoIndireta';
+import ListaFlex from './componentes/ListaFlex';
 
 const Drawer = createDrawerNavigator();
 
@@ -16,7 +19,10 @@ export default function App() {
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Home">
-      <Drawer.Screen name="Evento" component={() => <Evento />} />
+        <Drawer.Screen name="Lista Flex" component={ListaFlex} />
+        <Drawer.Screen name="Comunicação indireta" component={() => <TextoSincronizado />} />
+        <Drawer.Screen name="Comunicação direta" component={() => <Avo nome="João" sobrenome="Silva"/>} />
+        <Drawer.Screen name="Evento" component={() => <Evento />} />
         <Drawer.Screen name="ValidarProps" component={() => <ValidarProps ano={18} />} />
         <Drawer.Screen name="Plataforma" component={() => <Plataforma />} />
         <Drawer.Screen name="Contador" component={() => <Contador numeroInicial={50} />} />
